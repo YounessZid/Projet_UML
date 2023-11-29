cdd05d23ef0b576b6b790dc61c524ce2f8af7207
YounessZid <youness.zidane@efrei.net>
2023-11-29 12:46:53 +0200

    My base code

---
+++ src/main/java/fr/efrei/domain/Customer.java
@@ -0,0 +1,50 @@
+package fr.efrei.domain;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+
+    public Customer() {
+    }
+
+    private Customer(Builder builder) {
+        firstName = builder.firstName;
+        lastName = builder.lastName;
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+    private String getLastName() {
+        return lastName;
+
+    }
+    @Override
+    public String toString() {
+        return "Members{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+        public Customer build() {
+            return new Customer(this);
+        }
+    }
+}
--- src/main/java/fr/efrei/domain/Members.java
+++ src/main/java/fr/efrei/domain/Members.java
@@ -1,42 +1,8 @@
 package fr.efrei.domain;
 
-public class Members {
-    private String firstName;
-    private String lastName;
-    private Members() {}
+public class Members extends Customer{
+    private String subscription;
+    private String monthCheckIn;
+    private String conditions;
 
-    private Members(Builder builder) {
-        firstName = builder.firstName;
-        lastName = builder.lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-    private String getLastName() {
-        return lastName;
-
-    }
-    @Override
-    public String toString() {
-        return "Members{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
-    public static class Builder {
-        private String firstName;
-        private String lastName;
-        public Builder setFirstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-        public Builder setLastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-        public Members build() {
-            return new Members(this);
-        }
-    }
 }
