3aef3343d867bc8b12cc945b25bb9745d5ed2fce
YounessZid <youness.zidane@efrei.net>
2023-11-30 16:24:47 +0200

    member change

--- src/main/java/fr/efrei/domain/MonthCheckIn.java
+++ src/main/java/fr/efrei/domain/MonthCheckIn.java
@@ -1,22 +1,46 @@
+
 package fr.efrei.domain;
 
 
 import java.util.Date;
 
-public class MonthCheckIn extends Members {
+public class MonthCheckIn {
 
     private static final int MIN_CHECK_INS_REQUIRED = 3;
     private Date checkInDate;
     private Members member;
 
-    private MonthCheckIn(MonthCheckIn.Builder builder){
-        super(builder);
-        this.checkInDate = builder.checkInDate;
+    public MonthCheckIn(Date checkInDate, Members member) {
+        this.checkInDate = checkInDate;
+        this.member = member;
     }
 
     public Date getCheckInDate() {
         return checkInDate;
     }
+    public Members getMember() {
+        return member;
+    }
+
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public void setMember(Members member) {
+        this.member = member;
+    }
+
+
+    @Override
+    public String toString() {
+        return "MonthCheckIn{" +
+                "member=" + getMember() +
+                ", checkInDate=" + getCheckInDate() +
+                //", isValid=" + isCheckInValid() +
+                '}';
+    }
+
 
     /*public boolean isCheckInValid() {
         // Assuming check-ins should be counted only for the current month
@@ -28,34 +52,4 @@ public class MonthCheckIn extends Members {
         return false;
     }*/
 
-
-
-    @Override
-    public String toString() {
-        return "MonthCheckIn{" +
-                "subscription='" + getSubscription() + '\'' +
-                ", monthCheckIn='" + getMonthCheckIn() + '\'' +
-                ", conditions='" + getConditions() + '\'' +
-                ", customer=" + getCustomer() +
-                ", checkInDate=" + getCheckInDate() +
-                //", isValid=" + isCheckInValid() +
-                '}';
-    }
-
-    public static class Builder extends Members.Builder {
-        private Date checkInDate;
-
-        public Builder setCheckInDate(Date checkInDate) {
-            this.checkInDate = checkInDate;
-
-            return this;
-        }
-
-        @Override
-        public MonthCheckIn build() {
-            return new MonthCheckIn(this);
-        }
-    }
 }
-
-
--- src/main/java/fr/efrei/domain/Subscription.java
+++ src/main/java/fr/efrei/domain/Subscription.java
@@ -13,8 +13,102 @@ public class Subscription {
     private String monthCheckIn;
     private Customer customer;
 
+    public Subscription(String description, String duration, String name, int price, Date startDate, ArrayList<String> advantages, ArrayList<String> conditions, String monthCheckIn, Customer customer) {
+        this.description = description;
+        this.duration = duration;
+        this.name = name;
+        this.price = price;
+        this.startDate = startDate;
+        this.advantages = advantages;
+        this.conditions = conditions;
+        this.monthCheckIn = monthCheckIn;
+        this.customer = customer;
+    }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public String getDuration() {
+        return duration;
+    }
 
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public ArrayList<String> getAdvantages() {
+        return advantages;
+    }
+
+    public void setAdvantages(ArrayList<String> advantages) {
+        this.advantages = advantages;
+    }
+
+    public ArrayList<String> getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(ArrayList<String> conditions) {
+        this.conditions = conditions;
+    }
+
+    public String getMonthCheckIn() {
+        return monthCheckIn;
+    }
+
+    public void setMonthCheckIn(String monthCheckIn) {
+        this.monthCheckIn = monthCheckIn;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Subscription{" +
+                "description='" + description + '\'' +
+                ", duration='" + duration + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", startDate=" + startDate +
+                ", advantages=" + advantages +
+                ", conditions=" + conditions +
+                ", monthCheckIn='" + monthCheckIn + '\'' +
+                ", customer=" + customer +
+                '}';
+    }
 }
